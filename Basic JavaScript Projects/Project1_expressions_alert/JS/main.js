e34cb78ecdd25ca6865e254e355aa5d5e98aaf8f
Yikoro <jgibson1237777@gmail.com>
2025-11-19 15:39:47 -0500

    Javascript project assignment submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+document.write('Hello, world');
+var S = "Soup";
+document.write(S);
+var L = ('Mizu is water');
+window.alert(L); //Shows the variable "L" as a window alert popup//
+document.write("Miku said, \"Freedom is a lie?...\""); //Using "\" allows for ' and "" withing already existing quotations//
+document.write("\"I\'m hungry\" says the cat."
+    + "\"Then eat dummy\" says the dog."
+)
+var z = "Dragon" + ",Ravens are cool like Dragons."
+document.write(z);
+var cat = "Kuro", dog = "Shiro", raven = "ao" //multiple variables in one line//
+document.write(raven);
+document.write(20 + 20); //Math//
